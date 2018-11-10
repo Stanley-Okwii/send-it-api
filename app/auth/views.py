65c467c0fc3365194cf3d27f309895cc854dbb19
Stanley-Okwii <stanleeparker12@gmail.com>
2018-11-10 17:49:47 +0300

    [Update] fix unit tests

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,7 +3,6 @@ from flask_restful import reqparse, Resource
 from app.common.data import user_list
 from app.common.util import get_specific_user, \
     abort_if_user_does_not_exist, \
-    abort_if_content_type_is_not_json, \
     abort_if_email_does_not_match_type_email, \
     abort_if_password_is_less_than_4_characters
 
@@ -14,51 +13,45 @@ parser.add_argument('password')
 
 class Welcome(Resource):
     def get(self):
-        abort_if_content_type_is_not_json()
         return { "message": "welcome to send it api v1" }, 200
 
 class UserList(Resource):
     def get(self):
-        abort_if_content_type_is_not_json()
         return user_list, 200
 
 class User(Resource):
     def get(self, email):
-        abort_if_content_type_is_not_json()
         abort_if_email_does_not_match_type_email(email)
         abort_if_user_does_not_exist(email)
 
         return get_specific_user(email), 200
 
     def delete(self, email):
-        abort_if_content_type_is_not_json()
         abort_if_email_does_not_match_type_email(email)
         abort_if_user_does_not_exist(email)
         user_to_delete = get_specific_user(email)
         user_list.remove(user_to_delete)
-        del user_to_delete
-        return { "message": "user account deleted" }, 200
+
+        return { "message": "user account deleted" }, 204
 
     def put(self, email):
-        abort_if_content_type_is_not_json()
         abort_if_email_does_not_match_type_email(email)
         user = get_specific_user(email)
         abort_if_user_does_not_exist(email)
         args = parser.parse_args()
-        # password = args["password"]
-        # abort_if_password_is_less_than_4_characters(password)
-        newUser = { 'name': args['name'], "email": user["email"], "password": args["password"] }
+        password = args["password"]
+        abort_if_password_is_less_than_4_characters(password)
+        newUser = { 'name': args['name'], "email": user["email"], "password": password }
         user_list[user_list.index(user)] = newUser
 
-        # return {"message": "successfully updated account details"}, 201
-        return newUser, 201
+        return { "message": "successfully updated account details" }, 201
+
 
 
     def post(self):
-        abort_if_content_type_is_not_json()
         args = parser.parse_args()
-        # abort_if_email_does_not_match_type_email(args["email"])
-        # abort_if_password_is_less_than_4_characters(args["password"])
+        abort_if_email_does_not_match_type_email(args["email"])
+        abort_if_password_is_less_than_4_characters(args["password"])
         newUser = { 'name': args['name'], "email": args["email"], "password": args["password"] }
         user_list.append(newUser)
 
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,5 +1,6 @@
 import unittest
 from tests.base import BaseTestCase
+import json
 
 class TestAuth(BaseTestCase):
     def test_show_welcome_message(self):
@@ -38,16 +39,74 @@ class TestAuth(BaseTestCase):
             self.assertTrue(data['message'] == 'successfully created new user account')
             self.assertEqual(response.status_code, 201)
 
+    def test_update_of_user_information(self):
+        """
+        Test a user can update their account name and password
+        :return:
+        """
+        with self.client:
+            self.register_new_user("joyce","joyce@gmail.com","000000")
+            response = self.client.put(
+                'api/v1/user/joyce@gmail.com',
+                data=json.dumps(dict(name="superstar", password="007007"))
+            )
+
+            self.assertEqual(response.status_code, 201)
+
     def test_delete_user(self):
         """
-        Test a user is successfully created through the api
+        Test a user is successfully deleted
         :return:
         """
         with self.client:
             response = self.client.delete(
-                'api/v1/user/stanley@gmail.com',
-                content_type='application/json'
+                'api/v1/user/stanley@gmail.com'
             )
+
             self.assertEqual(response.status_code, 204)
 
+    def test_fetch_specific_user(self):
+        """
+        Test to get details of a given user
+        :return:
+        """
+        with self.client:
+            response = self.client.get(
+                'api/v1/user/okwii@gmail.com'
+            )
 
+            self.assertEqual(response.status_code, 200)
+
+    def test_fetch_user_with_incorrect_email_format(self):
+        """
+        Test to get details of a given user with a wrong email format
+        :return:
+        """
+        with self.client:
+            response = self.client.get(
+                'api/v1/user/owiigmail.com'
+            )
+
+            self.assertEqual(response.status_code, 400)
+
+    def test_fetch_user_who_is_not_registered(self):
+        """
+        Test to get details of a given user with a wrong email format
+        :return:
+        """
+        with self.client:
+            response = self.client.get(
+                'api/v1/user/andries@gmail.com'
+            )
+
+            self.assertEqual(response.status_code, 404)
+
+    def test_register_new_user_with_password_less_than_4_characters(self):
+        """
+        Test to create user with a password of less than 4 characters
+        :return:
+        """
+        with self.client:
+            response = self.register_new_user("wrong_user", "wrong_user@gmail.com", "123")
+
+            self.assertEqual(response.status_code, 400)
