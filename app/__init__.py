f8d00f18fb5f57141586d74e69a50304856af4d0
Stanley-Okwii <stanleeparker12@gmail.com>
2018-11-10 04:25:14 +0300

    [New] create welcome end point

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+import os
+from flask import Flask
+from flask_restful import Api
+from app.auth.views import User, UserList, Welcome
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(UserList, "/api/v1/users")
+api.add_resource(Welcome, "/api/v1", "/")
+api.add_resource(User, '/api/v1/user/<email>', '/api/v1/user')
