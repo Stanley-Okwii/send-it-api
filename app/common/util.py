e92759d250012841323b0ca9df42def4fdae1bfa
Stanley-Okwii <stanleeparker12@gmail.com>
2018-11-13 15:36:06 +0300

    [Update] add abort() using default flask

--- app/common/util.py
+++ app/common/util.py
@@ -16,17 +16,15 @@ def get_specific_user(email):
 def abort_if_user_does_not_exist(email):
     user = get_specific_user(email)
     if user == False:
-        return response("user with email {0} doesn't exist".format(email), 404)
+        abort(make_response(jsonify(message="user with email {0} doesn't exist".format(email)), 404))
 
 def abort_if_email_does_not_match_type_email(email):
     if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
-        return response("missing or incorrect email format", 400)
-        # abort(400)
-        # TODO: Get a better solution to handle exceptions
+        abort(make_response(jsonify(message="missing or incorrect email format"), 400))
 
 def abort_if_password_is_less_than_4_characters(password):
     if (len(str(password)) < 4):
-        return response("password is missing or less than 4 characters", 400)
+        abort(make_response(jsonify(message="password is missing or less than 4 characters"), 400))
 
 def get_specific_parcel(email, orderId):
     return next((item for item in parcel_delivery_orders[email] if item["id"] == orderId), False)
@@ -34,8 +32,8 @@ def get_specific_parcel(email, orderId):
 def abort_if_parcel_does_not_exist(email, orderId):
     parcel = get_specific_parcel(email, orderId)
     if parcel == False:
-        return response("parcel order with id {0} does not exist".format(orderId), 404)
+        abort(make_response(jsonify(message="parcel order with id {0} does not exist".format(orderId)), 404))
 
 def abort_if_user_does_not_have_orders(email):
     if (len(parcel_delivery_orders[email]) == 0):
-        return response("User with email {0} does not have any orders".format(email), 404)
+        abort(make_response(jsonify(message="User with email {0} does not have any orders".format(email)), 404))
