661658580e8cb671385a804b1c647db65928b0b8
Stanley-Okwii <stanleeparker12@gmail.com>
2018-11-26 21:33:55 +0300

    Add heroku default URL

--- app/models.py
+++ app/models.py
@@ -7,7 +7,6 @@ from dotenv import load_dotenv
 APP_ROOT = os.path.join(os.path.dirname(__file__))
 dotenv_path = os.path.join(APP_ROOT, '.env')
 load_dotenv(dotenv_path)
-
 API_ENVIRONMENT = os.getenv('API_ENV')
 
 
