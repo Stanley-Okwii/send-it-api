750ef85ad1476c0858dbb162fc5e397e33dda054
Stanley-Okwii <stanleeparker12@gmail.com>
2018-11-29 11:05:54 +0300

    Format sign in response messages

--- app/send_it/sign_in.py
+++ app/send_it/sign_in.py
@@ -1,17 +1,13 @@
-from flask import jsonify, request
+from flask import jsonify, request, make_response
 from flasgger import swag_from
 import datetime
 from flask.views import MethodView
 from app.common.util import (
     get_specific_user,
-    abort_if_user_does_not_exist,
-    abort_if_email_does_not_match_type_email,
-    abort_if_password_is_less_than_4_characters,
     response,
     abort_if_content_type_is_not_json,
     abort_if_user_input_is_missing,
-    abort_if_attribute_is_empty,
-    process_response_data
+    abort_if_attribute_is_empty
     )
 from flask_jwt_extended import (
     create_access_token
@@ -28,9 +24,6 @@ class SignIn(MethodView):
         email = args['email']
         abort_if_attribute_is_empty("email", email)
         abort_if_attribute_is_empty("password", password)
-        abort_if_email_does_not_match_type_email(email)
-        abort_if_password_is_less_than_4_characters(password)
-        abort_if_user_does_not_exist(email)
         user = get_specific_user(email)
         user_identity = {
             'name': user['username'],
@@ -46,6 +39,6 @@ class SignIn(MethodView):
                 'user_token': access_token
                 }
         if user['password'] == password:
-            return process_response_data(user_response, 200)
+            return make_response(jsonify(user_response)), 200
         else:
             return response('Invalid email or password, Please try again', 401)
